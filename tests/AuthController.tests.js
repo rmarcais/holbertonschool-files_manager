143a8ca32a4f7a376841f104b5f58d30cba0d56c
Elodie Riou <elodie.riou@hotmail.com>
2023-04-21 15:17:24 +0200

    FEATURE: implement tests for /disconnect route

--- tests/AuthController.tests.js
+++ tests/AuthController.tests.js
@@ -2,30 +2,30 @@ import {
   expect, request, use,
 } from 'chai';
 import chaiHttp from 'chai-http';
-import { ObjectId } from 'mongodb';
 import dbClient from '../utils/db';
 
 use(chaiHttp);
 const url = 'http://0.0.0.0:5000';
 
 describe('authController tests', () => {
+  const user = {
+    email: 'holberton@school.com',
+    password: 'helloC#16',
+  };
+
+  beforeEach(async () => {
+    await new dbClient.constructor();
+    await request(url).post('/users').send(user);
+  });
+
+  afterEach(async () => {
+    await dbClient.db.collection('users').deleteOne({ email: user.email });
+  });
+
+  const authValue = 'Basic aG9sYmVydG9uQHNjaG9vbC5jb206aGVsbG9DIzE2';
+
   describe('[GET] /connect route', () => {
-    const user = {
-      email: 'holberton@school.com',
-      password: 'helloC#16',
-    };
-
-    beforeEach(async () => {
-      await new dbClient.constructor();
-      await request(url).post('/users').send(user);
-    });
-
-    afterEach(async () => {
-      await dbClient.db.collection('users').deleteOne({ email: user.email });
-    });
-
     it('should return a token if find user', async () => {
-      const authValue = 'Basic aG9sYmVydG9uQHNjaG9vbC5jb206aGVsbG9DIzE2';
       const response = await request(url).get('/connect').set('authorization', authValue).send();
       expect(response.body).to.have.property('token');
       expect(response.statusCode).equals(200);
@@ -34,6 +34,24 @@ describe('authController tests', () => {
     it('should return an error if user not find', async () => {
       const errorAuthValue = 'Basic aG9sYmVydG9uQHNjaG9vbC5jb206aGVsbG9DIzE3';
       const response = await request(url).get('/connect').set('authorization', errorAuthValue).send();
+      expect(response.body).to.deep.equal({ error: 'Unauthorized' });
+      expect(response.statusCode).equals(401);
+    });
+  });
+
+  describe('[GET] /disconnect route', () => {
+    it('should delete the token and return nothing', async () => {
+      const connectResponse = await request(url).get('/connect').set('authorization', authValue).send();
+      const { token } = connectResponse.body;
+      const disconnectResponse = await request(url).get('/disconnect').set('x-token', token).send();
+      expect(disconnectResponse.request.header['x-token']).equals(token);
+      expect(disconnectResponse.body).to.deep.equal({});
+      expect(disconnectResponse.statusCode).equals(204);
+    });
+
+    it('should return an error if fake token is passed', async () => {
+      const errorToken = '04ced7ff-e339-4646-872d-783525fc145b';
+      const response = await request(url).get('/disconnect').set('x-token', errorToken).send();
       expect(response.body).to.deep.equal({ error: 'Unauthorized' });
       expect(response.statusCode).equals(401);
     });
