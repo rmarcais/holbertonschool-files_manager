cf8759d2c0ad7211abeaaf093fa59e200d6edcfb
Elodie Riou <elodie.riou@hotmail.com>
2023-04-21 10:34:20 +0200

    FEATURE: implement tests for routes /status and /stats

--- tests/AppController.tests.js
+++ tests/AppController.tests.js
@@ -1,30 +1,33 @@
-import { expect, assert } from 'chai';
-import request from 'request';
-import sinon from 'sinon';
-import AppController from '../controllers/AppControllers';
+import {
+  expect, request, use,
+} from 'chai';
+import chaiHttp from 'chai-http';
+import dbClient from '../utils/db';
 
-const sandbox = sinon.createSandbox();
-
+use(chaiHttp);
+const url = 'http://0.0.0.0:5000';
 describe('appController tests', () => {
-
-  afterEach(() => {
-    sandbox.restore();
+  describe('[GET] /status route', () => {
+    it('should return a status code 200', async () => {
+      const response = await request(url).get('/status').send();
+      expect(response.statusCode).equals(200);
+    });
+    it('should return the body with connection to redis and db', async () => {
+      const response = await request(url).get('/status').send();
+      expect(response.body).to.deep.equal({ redis: true, db: true });
+    });
   });
 
-  describe('[GET] /status route', () => {
-    it('should return a status code 200 and send body', () => {
-      const requestStub = sandbox
-        .stub(request, 'get')
-        .yields(null, { statusCode: 200 }, '{"redis":true,"db":true}');
-      AppController.getStatus({}, {
-        status(statusCode) {
-          expect(statusCode).equals(200);
-        },
-        send(body) {
-          expect(body).to.deep.equals({ redis: true, db: true });
-        },
-      });
-      requestStub.restore();
+  describe('[GET] /stats route', () => {
+    it('should return a status code 200', async () => {
+      const response = await request(url).get('/stats').send();
+      expect(response.statusCode).equals(200);
+    });
+    it('should return the body with nbUsers and nbFiles', async () => {
+      const users = await dbClient.nbUsers();
+      const files = await dbClient.nbFiles();
+      const response = await request(url).get('/stats').send();
+      expect(response.body).to.deep.equal({ users, files });
     });
   });
 });
