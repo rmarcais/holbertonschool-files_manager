b960f1ab355b43163f01c19ee964797907be356d
Rémi Marçais <remi.marcais87@gmail.com>
2023-04-22 12:32:51 +0200

    Fix issue

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,6 @@
 import express from 'express';
 import AppController from '../controllers/AppController';
-import UserController from '../controllers/UserController';
+import UserController from '../controllers/UsersController';
 import AuthController from '../controllers/AuthController';
 import FilesController from '../controllers/FilesController';
 
